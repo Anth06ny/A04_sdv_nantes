f533d44bb4217a5ee15837e0be6ab4f0facf3b6c
AnthonyPCPortable <anth06ny@gmail.com>
2026-04-08 15:01:34 +0200

    Fon TP Exception

--- app/src/main/java/com/example/a04_sdv_nantes/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/a04_sdv_nantes/presentation/viewmodel/MainViewModel.kt
@@ -5,21 +5,21 @@ import androidx.lifecycle.viewModelScope
 import com.example.a04_sdv_nantes.data.remote.KtorWeatherApi
 import com.example.a04_sdv_nantes.data.remote.WeatherEntity
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
-fun main(){
+fun main() {
     val viewModel = MainViewModel()
-    viewModel.loadWeathers("Nice")
+    viewModel.loadWeathers("Paris")
 
 
-    while(viewModel.runInProgress.value) {
+    while (viewModel.runInProgress.value) {
         println("Attente ...")
         Thread.sleep(500)
     }
 
-    println("List : ${viewModel.dataList.value}" )
+    println("List : ${viewModel.dataList.value}")
+    println("ErrorMessage : ${viewModel.errorMessage.value}" )
 
     //Pour que le programme s'arrête, inutile sur Android
     KtorWeatherApi.close()
@@ -29,16 +29,28 @@ class MainViewModel : ViewModel() {
     //MutableStateFlow est une donnée observable
     val dataList = MutableStateFlow(emptyList<WeatherEntity>())
     val runInProgress = MutableStateFlow(false)
+    val errorMessage = MutableStateFlow("")
 
-    fun loadWeathers(cityName:String){
+    fun loadWeathers(cityName: String) {
 
         runInProgress.value = true
+        errorMessage.value = ""
 
         viewModelScope.launch(Dispatchers.IO) {
-//Chercher les données
-            dataList.value = KtorWeatherApi.loadWeathers(cityName)
+            try {
+                if (cityName.length < 3) {
+                    throw Exception("Il faut au moins 3 caractères")
+                }
 
-            runInProgress.value = false
+//Chercher les données
+                dataList.value = KtorWeatherApi.loadWeathers(cityName)
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+                errorMessage.value = e.message ?: "Une erreur générique"
+            } finally {
+                runInProgress.value = false
+            }
         }
     }
 }
