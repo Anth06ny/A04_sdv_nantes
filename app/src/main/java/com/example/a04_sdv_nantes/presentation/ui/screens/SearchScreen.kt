b314b6382a7ee3767d725c88210d09e9b0d70b63
AnthonyPCPortable <anth06ny@gmail.com>
2026-04-09 10:39:26 +0200

    Ajout Thème

--- app/src/main/java/com/example/a04_sdv_nantes/presentation/ui/screens/SearchScreen.kt
+++ app/src/main/java/com/example/a04_sdv_nantes/presentation/ui/screens/SearchScreen.kt
@@ -5,23 +5,34 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil3.compose.AsyncImage
+import com.example.a04_sdv_nantes.R
+import com.example.a04_sdv_nantes.data.remote.WeatherEntity
 import com.example.a04_sdv_nantes.presentation.ui.theme.A04_sdv_nantesTheme
 import com.example.a04_sdv_nantes.presentation.viewmodel.MainViewModel
 
 @Preview(showBackground = true, showSystemUi = true)
-@Preview(showBackground = true, showSystemUi = true,
-    uiMode = android.content.res.Configuration.UI_MODE_NIGHT_YES or android.content.res.Configuration.UI_MODE_TYPE_NORMAL)
+@Preview(
+    showBackground = true, showSystemUi = true,
+    uiMode = android.content.res.Configuration.UI_MODE_NIGHT_YES or android.content.res.Configuration.UI_MODE_TYPE_NORMAL
+)
 @Composable
 fun SearchScreenPreview() {
     //Il faut remplacer NomVotreAppliTheme par le thème de votre application
@@ -35,19 +46,53 @@ fun SearchScreenPreview() {
 
 @Composable
 fun SearchScreen(modifier: Modifier = Modifier, viewModel: MainViewModel = MainViewModel()) {
-    Column(modifier= modifier) {
+    Column(
+        modifier = modifier
+            .background(MaterialTheme.colorScheme.background)
+            .fillMaxSize()
+    ) {
         println("SearchScreen()")
-        Text(text = "Text1",fontSize = 20.sp, modifier = Modifier.background(Color.Blue))
+
         Spacer(Modifier.size(8.dp))
-        Text(text = "Text2",fontSize = 14.sp)
+
 
         viewModel.dataList.collectAsStateWithLifecycle().value.forEach {
-            PictureRowItem(it.name)
+            PictureRowItem(data = it)
         }
     }
 }
 
-@Composable
-fun PictureRowItem(text:String){
-    Text(text = text,fontSize = 20.sp, color = Color.Blue)
+@Composable //Composable affichant 1 élément
+fun PictureRowItem(modifier: Modifier = Modifier, data: WeatherEntity) {
+    Row(modifier = modifier
+        .fillMaxWidth()
+        .background(MaterialTheme.colorScheme.secondaryContainer)
+        .padding(4.dp)) {
+        //Permission Internet nécessaire
+        AsyncImage(
+            model = data.weather.firstOrNull()?.icon ?: "",
+            //Pour aller le chercher dans string.xml R de votre package com.nom.projet
+            //contentDescription = getString(R.string.picture_of_cat),
+            //En dur
+            contentDescription = "une photo de chat",
+            contentScale = ContentScale.FillWidth,
+
+            //Pour toto.png. Si besoin de choisir l'import pour la classe R, c'est celle de votre package
+            //Image d'échec de chargement qui sera utilisé par la preview
+            error = painterResource(R.drawable.error),
+            //Image d'attente.
+            //placeholder = painterResource(R.drawable.toto),
+
+            onError = { println(it) },
+            modifier = Modifier
+                .heightIn(max = 100.dp)
+                .widthIn(max = 100.dp)
+        )
+
+        Column(modifier = Modifier.padding(start = 4.dp)) {
+            Text(text = data.name, fontSize = 20.sp, color = MaterialTheme.colorScheme.primary)
+            Text(text = data.getResume().take(10) + "...", fontSize = 14.sp,
+                color = MaterialTheme.colorScheme.onSecondaryContainer)
+        }
+    }
 }
